c6dbbb0fcc3186913f26769535ecd843af63d57e
jesseyaks01 <jesseyakubu16@gmail.com>
2025-01-28 23:17:36 +0100

    Create SwingCalculator.java

---
+++ SwingCalculator.java
@@ -0,0 +1,307 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SwingCalculator extends JFrame implements ActionListener {
+    // Components
+    private JTextField display;
+    private JButton[] numberButtons = new JButton[10];
+    private JButton[] functionButtons = new JButton[8];
+    private JButton addButton, subButton, mulButton, divButton;
+    private JButton decButton, equButton, delButton, clrButton;
+    private JPanel panel;
+
+    // Variables
+    private double num1 = 0, num2 = 0, result = 0;
+    private char operator;
+
+    public SwingCalculator() {
+        // Frame settings
+        setTitle("Calculator");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 550);
+        setLayout(null);
+
+        // Display
+        display = new JTextField();
+        display.setBounds(50, 25, 300, 50);
+        display.setEditable(false);
+        add(display);
+
+        // Buttons
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Del");
+        clrButton = new JButton("Clear");
+
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+
+        for (int i = 0; i < 8; i++) {
+            functionButtons[i].addActionListener(this);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+        }
+
+        // Panel
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+
+        // Adding buttons to the panel
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numberButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
+
+        // Add panel and additional buttons
+        add(panel);
+        clrButton.setBounds(50, 420, 145, 50);
+        delButton.setBounds(205, 420, 145, 50);
+        add(clrButton);
+        add(delButton);
+
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < 10; i++) {
+            if (e.getSource() == numberButtons[i]) {
+                display.setText(display.getText().concat(String.valueOf(i)));
+            }
+        }
+        if (e.getSource() == decButton) {
+            display.setText(display.getText().concat("."));
+        }
+        if (e.getSource() == addButton) {
+            num1 = Double.parseDouble(display.getText());
+            operator = '+';
+            display.setText("");
+        }
+        if (e.getSource() == subButton) {
+            num1 = Double.parseDouble(display.getText());
+            operator = '-';
+            display.setText("");
+        }
+        if (e.getSource() == mulButton) {
+            num1 = Double.parseDouble(display.getText());
+            operator = '*';
+            display.setText("");
+        }
+        if (e.getSource() == divButton) {
+            num1 = Double.parseDouble(display.getText());
+            operator = '/';
+            display.setText("");
+        }
+        if (e.getSource() == equButton) {
+            num2 = Double.parseDouble(display.getText());
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num2 != 0 ? num1 / num2 : 0;
+                    break;
+            }
+            display.setText(String.valueOf(result));
+            num1 = result;
+        }
+        if (e.getSource() == clrButton) {
+            display.setText("");
+        }
+        if (e.getSource() == delButton) {
+            String text = display.getText();
+            display.setText(text.length() > 0 ? text.substring(0, text.length() - 1) : "");
+        }
+    }
+
+    public static void main(String[] args) {
+        new SwingCalculator();
+    }
+          }import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SwingCalculator extends JFrame implements ActionListener {
+    // Components
+    private JTextField display;
+    private JButton[] numberButtons = new JButton[10];
+    private JButton[] functionButtons = new JButton[8];
+    private JButton addButton, subButton, mulButton, divButton;
+    private JButton decButton, equButton, delButton, clrButton;
+    private JPanel panel;
+
+    // Variables
+    private double num1 = 0, num2 = 0, result = 0;
+    private char operator;
+
+    public SwingCalculator() {
+        // Frame settings
+        setTitle("Calculator");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 550);
+        setLayout(null);
+
+        // Display
+        display = new JTextField();
+        display.setBounds(50, 25, 300, 50);
+        display.setEditable(false);
+        add(display);
+
+        // Buttons
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Del");
+        clrButton = new JButton("Clear");
+
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+
+        for (int i = 0; i < 8; i++) {
+            functionButtons[i].addActionListener(this);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+        }
+
+        // Panel
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+
+        // Adding buttons to the panel
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numberButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
+
+        // Add panel and additional buttons
+        add(panel);
+        clrButton.setBounds(50, 420, 145, 50);
+        delButton.setBounds(205, 420, 145, 50);
+        add(clrButton);
+        add(delButton);
+
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < 10; i++) {
+            if (e.getSource() == numberButtons[i]) {
+                display.setText(display.getText().concat(String.valueOf(i)));
+            }
+        }
+        if (e.getSource() == decButton) {
+            display.setText(display.getText().concat("."));
+        }
+        if (e.getSource() == addButton) {
+            num1 = Double.parseDouble(display.getText());
+            operator = '+';
+            display.setText("");
+        }
+        if (e.getSource() == subButton) {
+            num1 = Double.parseDouble(display.getText());
+            operator = '-';
+            display.setText("");
+        }
+        if (e.getSource() == mulButton) {
+            num1 = Double.parseDouble(display.getText());
+            operator = '*';
+            display.setText("");
+        }
+        if (e.getSource() == divButton) {
+            num1 = Double.parseDouble(display.getText());
+            operator = '/';
+            display.setText("");
+        }
+        if (e.getSource() == equButton) {
+            num2 = Double.parseDouble(display.getText());
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num2 != 0 ? num1 / num2 : 0;
+                    break;
+            }
+            display.setText(String.valueOf(result));
+            num1 = result;
+        }
+        if (e.getSource() == clrButton) {
+            display.setText("");
+        }
+        if (e.getSource() == delButton) {
+            String text = display.getText();
+            display.setText(text.length() > 0 ? text.substring(0, text.length() - 1) : "");
+        }
+    }
+
+    public static void main(String[] args) {
+        new SwingCalculator();
+    }
+                  }
